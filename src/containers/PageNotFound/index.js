ee858bf4d8b3856a8addbae344e13649e719c0e2
Trần Hiếu <83171071+dacvuips@users.noreply.github.com>
2021-10-26 23:27:52 +0700

    Update index.js

--- src/containers/PageNotFound/index.js
+++ src/containers/PageNotFound/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-
+import NavMovie from "../HomeTemplate/HomePage/NavMovie/index";
+import Header from "../HomeTemplate/HomePage/Header";
 export default class PageNotFound extends Component {
   render() {
     return (
-      <div>
-        <h3>PageNotFound</h3>
-      </div>
+      <>
+        <Header />
+        <NavMovie />
+        <img
+          style={{ width: "100%", height: "100%", marginTop: "90px" }}
+          src="https://thanhbinhpc.com/wp-content/uploads/2019/03/sua-loi-404-tren-may-tinh.1PNG-1.png"
+          atl="https://thanhbinhpc.com/wp-content/uploads/2019/03/sua-loi-404-tren-may-tinh.1PNG-1.png"
+        />
+      </>
     );
   }
 }
