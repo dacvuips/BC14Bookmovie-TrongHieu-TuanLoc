d01fca6a4092147b515095fe0a0aa7526870cf9b
Trần Hiếu <83171071+dacvuips@users.noreply.github.com>
2021-10-31 21:55:25 +0700

    Delete containers directory

--- containers/AdminTemplate/UserManager/module/constants.js
+++
@@ -1,4 +0,0 @@
-export const GET_USER_REQUEST = "@getUsermeducer/GET_USER_REQUEST";
-export const GET_USER_SUCCESS = "@getUsermReducer/GET_USER_SUCCESS";
-export const GET_USER_FAILED = "@getUsermReducer/GET_USER_FAILED";
-export const SET_USER_REQUEST = "@getUsermReducer/SET_USER_REQUEST";
--- containers/HomeTemplate/DetailMoviePage/modules/constants.js
+++
@@ -1,6 +0,0 @@
-export const DETAIL_MOVIE_REQUEST = "@detailMovieReducer/DETAIL_MOVIE_REQUEST";
-export const DETAIL_MOVIE_SUCCESS = "@detailMovieReducer/DETAIL_MOVIE_SUCCESS";
-export const DETAIL_MOVIE_FAILED = "@detailMovieReducer/DETAIL_MOVIE_FAILED";
-export const LICH_CHIEU_REQUEST = "@lichChieuReducer/LICH_CHIEU_REQUEST";
-export const LICH_CHIEU_SUCCESS = "@lichChieuReducer/LICH_CHIEU_SUCCESS";
-export const LICH_CHIEU_FAILED = "@lichChieuReducer/LICH_CHIEU_FAILED";
